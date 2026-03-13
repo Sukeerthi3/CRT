7d6fc646622c40620520b5acb5c1be4ddce8af2d
Sukeerthi3 <gajulakeerthi3@gmail.com>
2026-03-13 15:21:55 +0530

    M02_Assignments_completed

--- IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST01/task.py
+++ IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST01/task.py
@@ -1,5 +1,9 @@
 def count_digits(n: int) -> int:
-    return len(str(abs(n)))
+    count = 0
+    while n >0:
+        n = n//10
+        count = count+1
+    return count
 
 if __name__ == "_main_":
     n = int(input())
